2d66ccf61bd393d24d2c47fad3c2d0fd09493cb4
liufei <demoliu94@163.com>
2022-05-09 15:25:59 +0800

    增加渐变背景切换

---
+++ Constant/BGStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 背景风格
+/// </summary>
+namespace GeekDesk.Constant
+{
+    public enum BGStyle
+    {
+        ImgBac = 1,  //图片
+        GradientBac = 2  //渐变色
+    }
+}
