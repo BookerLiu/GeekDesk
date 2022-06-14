896dc26f11a62b3de7050e5a2eaf4a6ce5b6e364
liufei <demoliu94@163.com>
2022-06-14 09:48:13 +0800

    :bug: 修复路径可能为空导致创建相对路径报错的问题

--- Util/FileUtil.cs
+++ Util/FileUtil.cs
@@ -159,21 +159,7 @@ namespace GeekDesk.Util
 
         public static string MakeRelativePath(string fromPath, string toPath)
         {
-            //if (string.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
-            //if (string.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
-
-            //Uri fromUri = new Uri(fromPath);
-            //Uri toUri = new Uri(toPath);
-
-            //if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
-
-            //Uri relativeUri = fromUri.MakeRelativeUri(toUri);
-            //string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
-
-            //if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
-            //{
-            //    relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
-            //}
+            if (string.IsNullOrEmpty(toPath) || string.IsNullOrEmpty(fromPath)) return null;
             Uri file = new Uri(@toPath);
             // Must end in a slash to indicate folder
             Uri folder = new Uri(@fromPath);
