ea39576709f8e7750e09f42d7bf140e81ea60c16
BookerLiu <booker_liu@163.com>
2023-04-21 14:16:58 +0800

    :poop: 防止无法弹出更新窗口

--- MyThread/UpdateThread.cs
+++ MyThread/UpdateThread.cs
@@ -51,14 +51,18 @@ namespace GeekDesk.MyThread
                 {
                     JObject jo = JObject.Parse(updateInfo);
 
-
-                    string statisticUrl = jo["statisticUrl"].ToString();
-                    if (!string.IsNullOrEmpty(statisticUrl))
+                    try
                     {
-                        //用户统计  只通过uuid统计用户数量  不收集任何信息
-                        statisticUrl += "?uuid=" + CommonCode.GetUniqueUUID();
-                        HttpUtil.Get(statisticUrl);
-                    }
+                        string statisticUrl = jo["statisticUrl"].ToString();
+                        if (!string.IsNullOrEmpty(statisticUrl))
+                        {
+                            //用户统计  只通过uuid统计用户数量  不收集任何信息
+                            statisticUrl += "?uuid=" + CommonCode.GetUniqueUUID();
+                            HttpUtil.Get(statisticUrl);
+                        }
+                    } catch (Exception){}
+
+                   
 
                     string onlineVersion = jo["version"].ToString();
                     if (onlineVersion.CompareTo(nowVersion) > 0)
